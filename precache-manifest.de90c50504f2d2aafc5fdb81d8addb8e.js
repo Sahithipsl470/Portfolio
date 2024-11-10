23a264e4c7f46021bb2d863137608874421a39a2
App-Dev-123 <madhavisahi2000@gmail.com>
2024-11-10 00:36:07 -0500

    new-commit

---
+++ precache-manifest.de90c50504f2d2aafc5fdb81d8addb8e.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fabfdfe95f2b1f08014adfdad9a2c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d90077da4831797b94",
+    "url": "/static/css/main.67b3b053.chunk.css"
+  },
+  {
+    "revision": "09509e004a0727ea347e",
+    "url": "/static/js/2.e1a2c42e.chunk.js"
+  },
+  {
+    "revision": "c5d90077da4831797b94",
+    "url": "/static/js/main.e719de50.chunk.js"
+  },
+  {
+    "revision": "9a0a6115f3bd06a10d72",
+    "url": "/static/js/runtime-main.f65c7a44.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "49e5c00d4c8c0c26eee71aabb19bff03",
+    "url": "/static/media/aaveg_logo.49e5c00d.jpeg"
+  },
+  {
+    "revision": "c158a2e027c7a2a51f3d978f9854f173",
+    "url": "/static/media/aws_logo.c158a2e0.png"
+  },
+  {
+    "revision": "3f65a3a338bdfec746e1ff609868969d",
+    "url": "/static/media/contact_me.3f65a3a3.jpg"
+  },
+  {
+    "revision": "713b2d436048d378701b2dd64bf3b7c3",
+    "url": "/static/media/corey_logo.713b2d43.png"
+  },
+  {
+    "revision": "bfaf3f0ccb537507bbd0e2b26c8a49eb",
+    "url": "/static/media/data_science_image.bfaf3f0c.jpg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "ec61a0718a544d948296934f31f3a7c8",
+    "url": "/static/media/experience_logo.ec61a071.jpg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "42233cf7c763d262a326b6ea0af5ab06",
+    "url": "/static/media/full_stack_image.42233cf7.jpg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "06b1acbde919e6a1f59cd02cd79d454e",
+    "url": "/static/media/micro_services_image.06b1acbd.jpg"
+  },
+  {
+    "revision": "2ceefbac77c6ccf2e6ab53b06c335dc1",
+    "url": "/static/media/nit_trichy_logo.2ceefbac.png"
+  },
+  {
+    "revision": "606dc8964b723756394d30561944dfbb",
+    "url": "/static/media/opening.606dc896.gif"
+  },
+  {
+    "revision": "7dbc6ffc147556aa1cfb08c4cd70cace",
+    "url": "/static/media/pragyan_logo.7dbc6ffc.png"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "cf632e6758cd090ceb1e25e2309ccb21",
+    "url": "/static/media/sahithi_image.cf632e67.jpg"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "ec07030e22503ae0727a24fe9e3b8c68",
+    "url": "/static/media/ww.ec07030e.png"
+  }
+]);
